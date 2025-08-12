4d73884c6723509bde47e285e10b165781c232f1
Garrett Lander <garrett.jlander@gmail.com>
2025-08-12 12:45:49 +0200

    part 1 lecture code added

---
+++ module04-intro-to-react/01-react-fundamentals-lecture/react/src/components/DuckForm.jsx
@@ -0,0 +1,41 @@
+const DuckForm = () => {
+	return (
+		<section className='justify-self-end flex flex-col items-center gap-4 border-2 rounded-lg p-4 mx-8'>
+			<h2 className='text-4xl'>Add a new duck to the pond!</h2>
+			<form id='add-form' className='flex flex-col gap-4 w-3/4'>
+				<label className='w-full flex gap-2 items-baseline'>
+					<span className='text-xl'>Name:</span>
+					<input
+						id='name'
+						type='text'
+						placeholder="What is your duck's name?"
+						className='bg-inherit border-solid border-2 border-slate-700 rounded-lg p-2 flex-grow'
+					/>
+				</label>
+				<label className='w-full flex gap-2 items-baseline'>
+					<span className='text-xl'>Image:</span>
+					<input
+						id='img-url'
+						type='text'
+						placeholder='What does your duck look like?'
+						className='bg-inherit border-solid border-2 border-slate-700 rounded-lg p-2 w-full'
+					/>
+				</label>
+				<label className='w-full flex gap-2 items-baseline'>
+					<span className='text-xl'>Quote:</span>
+					<input
+						id='quote'
+						type='text'
+						placeholder='What does your duck say?'
+						className='bg-inherit border-solid border-2 border-slate-700 rounded-lg p-2 w-full'
+					/>
+				</label>
+				<button type='submit' className='bg-green-600 p-2 rounded-lg font-bold'>
+					Add duck
+				</button>
+			</form>
+		</section>
+	);
+};
+
+export default DuckForm;
