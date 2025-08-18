0f9716b098117de70e3dd0eb6b627496cbcf996a
Garrett Lander <garrett.jlander@gmail.com>
2025-08-18 15:19:00 +0200

    thinking in react ii with challenges

---
+++ module04-intro-to-react/07-thinking-in-react-ii/src/components/FilterComponent.jsx
@@ -0,0 +1,31 @@
+const FilterComponent = ({ setFilter, filter }) => {
+	const setFilterInView = filter => {
+		// We'll refine this eventually
+		setFilter(filter);
+	};
+
+	return (
+		<div className='mb-4 flex space-x-2'>
+			<button
+				onClick={() => setFilterInView('all')}
+				className={`bg-gray-200 px-3 py-1 rounded ${filter == 'all' ? 'border-2 border-blue-500' : ''}`}
+			>
+				All
+			</button>
+			<button
+				onClick={() => setFilterInView('active')}
+				className={`bg-gray-200 px-3 py-1 rounded ${filter == 'active' ? 'border-2 border-blue-500' : ''}`}
+			>
+				Active
+			</button>
+			<button
+				onClick={() => setFilterInView('completed')}
+				className={`bg-gray-200 px-3 py-1 rounded ${filter == 'completed' ? 'border-2 border-blue-500' : ''}`}
+			>
+				Completed
+			</button>
+		</div>
+	);
+};
+
+export default FilterComponent;
