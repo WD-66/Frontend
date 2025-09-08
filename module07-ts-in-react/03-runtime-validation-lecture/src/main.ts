a954572851f2782d5a193e99256221322153be3b
anoj-kt <anojk28@gmail.com>
2025-09-08 14:24:46 +0200

    Added comments on zod lecture

--- module07-ts-in-react/03-runtime-validation-lecture/src/main.ts
+++ module07-ts-in-react/03-runtime-validation-lecture/src/main.ts
@@ -1,40 +1,48 @@
 import { z } from 'zod/v4';
 
+// Sample user object to validate
 const sampleUser = {
   id: 1,
   name: 'John',
   email: 'JOHN@gmail.com',
 };
 
-//Define a Zod Schema
+// Define a Zod schema for a user object
 const UserSchema = z.object({
-  id: z.number(),
-  name: z.string().min(1),
-  email: z.email('Pls enter a valid email').toLowerCase(),
-  //   isAdmin: z.boolean().default(false),
+  id: z.number(), // id must be a number
+  name: z.string().min(1), // name must be a non-empty string
+  email: z.email('Pls enter a valid email').toLowerCase(), // email must be valid and lowercase
+  // isAdmin: z.boolean().default(false), // (optional) boolean with default value
 });
 
+// Define a schema for an array of users
 const UserSchemaArray = z.array(UserSchema);
 
+// Infer TypeScript type from schema
 type User = z.infer<typeof UserSchema>;
 
-// const values = UserSchema.safeParse(sampleUser);
-// console.log(values);
+// Example of validating a single user
+const values = UserSchema.safeParse(sampleUser);
+console.log(values);
 
-// Values will have properties success, error, data
+//--------------------------
+
+// Function to fetch and validate users from an API
 const fetchUser = async () => {
   try {
     const response = await fetch('https://jsonplaceholder.typicode.com/users');
-
     const resData = await response.json();
 
+    // Validate the fetched data against the schema
     const { success, data, error } = UserSchemaArray.safeParse(resData);
 
     if (!success) {
+      // If validation fails, log the error
       console.log(error?.message);
       throw new Error('Failed to fetch');
     }
 
+    // If validation passes, return the data
     return data;
   } catch (error) {
     console.log(error);
